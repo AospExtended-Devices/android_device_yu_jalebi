0322a2069bd91866904024bb35dd2f36f5aea4e1
iamsj7 <shaikjaleel270@gmail.com>
2018-03-13 11:03:50 +0000

    jalebi: /system/bin --> /vendor/bin

--- product/media.mk
+++ product/media.mk
@@ -1,3 +1,3 @@
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml
